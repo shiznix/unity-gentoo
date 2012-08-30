8a72246f4ad0096d633bf84e02cad120a37222e3
Rick Harris <rickfharris@yahoo.com.au>
2012-08-30 13:44:26 +0930

    Improve version_check.sh script

--- version_check.sh
+++ version_check.sh
@@ -7,15 +7,16 @@
 version_check() {
 	packbasename=`basename ${pack} | awk -F.ebuild '{print $1}'`
 	packname=`echo ${pack} | awk -F/ '{print ( $(NF-1) )}'`
-	if [ -n "`echo "${packbasename}" | grep 'ccsm'`" ]; then packname="compizconfig-settings-manager"
-	elif [ -n "`echo "${packbasename}" | grep 'fixesproto'`" ]; then packname="x11proto-fixes"
-	elif [ -n "`echo "${packbasename}" | grep 'glib'`" ]; then packname="glib2.0"
-	elif [ -n "`echo "${packbasename}" | grep 'gtk+-99.2'`" ]; then packname="gtk+2.0"
-	elif [ -n "`echo "${packbasename}" | grep 'gtk+-99.3'`" ]; then packname="gtk+3.0"
-	elif [ -n "`echo "${packbasename}" | grep 'libXfixes'`" ]; then packname="libxfixes"
-	elif [ -n "`echo "${packbasename}" | grep 'lazr-restfulclient'`" ]; then packname="lazr.restfulclient"
-	elif [ -n "`echo "${packbasename}" | grep 'nm-applet'`" ]; then packname="network-manager-applet"
-	elif [ -n "`echo "${packbasename}" | grep 'unity2d'`" ]; then packname="unity-2d"
+	if [ -n "`echo "${packbasename}" | grep 'ccsm'`" ]; then treepackname="${packname}"; packname="compizconfig-settings-manager"
+	elif [ -n "`echo "${packbasename}" | grep 'fixesproto'`" ]; then treepackname="${packname}"; packname="x11proto-fixes"
+	elif [ -n "`echo "${packbasename}" | grep 'glib'`" ]; then treepackname="${packname}"; packname="glib2.0"
+	elif [ -n "`echo "${packbasename}" | grep 'gtk+-99.2'`" ]; then treepackname="${packname}"; packname="gtk+2.0"
+	elif [ -n "`echo "${packbasename}" | grep 'gtk+-99.3'`" ]; then treepackname="${packname}"; packname="gtk+3.0"
+	elif [ -n "`echo "${packbasename}" | grep 'libXfixes'`" ]; then treepackname="${packname}"; packname="libxfixes"
+	elif [ -n "`echo "${packbasename}" | grep 'lazr-restfulclient'`" ]; then treepackname="${packname}"; packname="lazr.restfulclient"
+	elif [ -n "`echo "${packbasename}" | grep 'nm-applet'`" ]; then treepackname="${packname}"; packname="network-manager-applet"
+	elif [ -n "`echo "${packbasename}" | grep 'unity2d'`" ]; then treepackname="${packname}"; packname="unity-2d"
+	elif [ -n "`echo "${packbasename}"`" ]; then treepackname="${packname}"
 	fi
 	. ${pack} &> /dev/null
 	if [ -n "${UVER}" ]; then
@@ -24,7 +25,9 @@ version_check() {
 		echo
 		echo "Checking http://packages.ubuntu.com/${URELEASE}/source/${packname}"
 		echo "Current version:  ${current}"
-		if [ "${current}" != "${upstream}" ]; then
+		current_version=`echo "${current}" | sed "s/^\${treepackname}-//"`
+		upstream_version=`echo "${upstream}" | sed "s/^\${packname}-//"`
+		if [ "${current_version}" != "${upstream_version}" ]; then
 			echo -e "Upstream version: \033[5m\033[1m${upstream}\033[0m"
 		else
 			echo "Upstream version: ${upstream}"
