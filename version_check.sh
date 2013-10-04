39732a9630963dd0ed9d4a5c498b7c989a706605
Rick Harris <rickfharris@yahoo.com.au>
2013-10-04 09:38:14 +0930

    [Saucy] Add 'mir' useflag patched x11-drivers/xf86-video-intel ebuild

--- version_check.sh
+++ version_check.sh
@@ -28,6 +28,7 @@ local_to_upstream_packnames() {
 	elif [ -n "`echo "${packbasename}" | grep 'qtpim'`" ]; then treepackname="${packname}"; packname="libqt5organizer5"
 	elif [ -n "`echo "${packbasename}" | grep 'unity-webapps'`" ]; then treepackname="${packname}"; packname="libunity-webapps"
 	elif [ -n "`echo "${packbasename}" | grep 'webapps-base'`" ]; then treepackname="${packname}"; packname="unity-webapps-common"
+	elif [ -n "`echo "${packbasename}" | grep 'xf86-video-intel'`" ]; then treepackname="${packname}"; packname="xserver-xorg-video-intel"
 	elif [ -n "`echo "${packbasename}"`" ]; then treepackname="${packname}"
 	fi
 }
